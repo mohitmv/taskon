11bdb7f0ba0f06e68c3d07c5bae469d0315a0d56
Mohit Saini <mohitsaini1196@gmail.com>
2020-07-21 23:06:15 +0530

    Improve documentation in scheduling_algorithm.py

--- taskon/scheduling_algorithm.py
+++ taskon/scheduling_algorithm.py
@@ -24,37 +24,31 @@ class SchedulingAlgorithm:
     Step-4: Calculate the skipped tasks and return.
     """
     def __init__(self, task_processor, task_inputs_func, tasks_map, deps_func):
+        """
+        @task_processor - An implementation of task processor. Refer to
+                          taskon/abstract_task_processor.py to know more.
+                          Task scheduler use @task_processor to schedule
+                          a task when all of its dependency tasks are executed
+                          successfully.
+        @task_inputs_func - A function that takes a task and return the real
+                            inputs (args, kwargs) of the task. Note that inputs
+                            of a task are declared by client but these input
+                            contains TaskResult(...) placeholder to denote the
+                            output of dependency task. This function takes the
+                            user declared inputs and replace the TaskResult
+                            placeholders with actual result of dependency tasks.
+                            Output format: (args, kwargs)
+        @tasks_map - A map from a unique task id -> Task object. We are
+                     referencing tasks by their unique id at various places,
+                     where it is essential to reference a task by its id.
+                     For eg: a set of tasks (task id is hashable by default).
+        @deps_func - A function that takes a task id and return a set of
+                     dependency task ids.
+        """
         self.task_processor = task_processor
         self.task_inputs_func = task_inputs_func
         self.tasks_map = tasks_map
         self.deps_func = deps_func
-
-    def __createRuntimeGraph(self, effective_tasks, deps_func):
-        incoming_edges = dict((i, set(deps_func(i))) for i in effective_tasks)
-        outgoing_edges = dict((i, []) for i in effective_tasks)
-        for i, deps in incoming_edges.items():
-            for d in deps:
-                outgoing_edges[d].append(i)
-        return incoming_edges, outgoing_edges
-
-    def __processTask(self, task):
-        """
-        Process the execution of @task in task_processor. This @task can be
-        executed by anytime now. At this point all the dependency tasks of the
-        @task have executed successfully, hence we can use 'task_inputs_func'
-        to get the real inputs of @task.
-        """
-        args, kwargs = self.task_inputs_func(task)
-        self.tasks_in_progress.add(task.id)
-        self.task_processor.process(
-            task, self.__onCompleteCallback, *args, **kwargs)
-
-    def __onCompleteCallback(self, task, status):
-        """
-        A callback to be called by task_processor after the completion of a
-        scheduled task.
-        """
-        self.completion_updates_queue.put((task, status))
 
     def run(self, effective_tasks, continue_on_failure=False):
         """The main scheduling algorithm."""
@@ -85,3 +79,34 @@ class SchedulingAlgorithm:
                 self.tasks_map[task_id].abort()
         self.task_processor.close()
 
+    def __createRuntimeGraph(self, effective_tasks, deps_func):
+        """
+        @effective_tasks - Set of task ids. effective tasks are the actual
+                           tasks we need to execute, derived from the
+                           dependency cover of target_tasks.
+        """
+        incoming_edges = dict((i, set(deps_func(i))) for i in effective_tasks)
+        outgoing_edges = dict((i, []) for i in effective_tasks)
+        for i, deps in incoming_edges.items():
+            for d in deps:
+                outgoing_edges[d].append(i)
+        return incoming_edges, outgoing_edges
+
+    def __processTask(self, task):
+        """
+        Process the execution of @task in task_processor. This @task can be
+        executed by anytime now. At this point all the dependency tasks of the
+        @task have executed successfully, hence we can use 'task_inputs_func'
+        to get the real inputs of @task.
+        """
+        args, kwargs = self.task_inputs_func(task)
+        self.tasks_in_progress.add(task.id)
+        self.task_processor.process(
+            task, self.__onCompleteCallback, *args, **kwargs)
+
+    def __onCompleteCallback(self, task, status):
+        """
+        A callback to be called by task_processor after the completion of a
+        scheduled task.
+        """
+        self.completion_updates_queue.put((task, status))
